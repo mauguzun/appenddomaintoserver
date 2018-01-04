cab8a4078d57bba26bd4fea6292e6b586e4448fd
Denis <mauguzun@gmail.com>
2018-01-04 16:15:53 +0200

    first

---
+++ ConsoleApp1/MakeAcc.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MakeAcc
+    {
+        string filename = "result.txt";
+        string error = "error.txt";
+        
+
+        public ChromeDriver dr;
+
+     
+
+
+
+        public MakeAcc(string url )
+        {
+            dr = new ChromeDriver();
+            dr.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 50));
+            dr.Url = url; 
+        }
+
+        internal void PutDomain(string url, string domain)
+        {
+           
+            dr.Url = url;
+
+            dr.FindElementById("domain").SendKeys(domain);
+            dr.FindElementById("subdomain").SendKeys(RandomString(7));
+            dr.FindElementById("dir").Clear();
+            dr.FindElementById("dir").SendKeys("public_html/base");
+            dr.FindElementByCssSelector("#submit_domain").Click();
+
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+
+      
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string url;
+        static void Main(string[] args)
+        {
+            MakeAcc acc = new MakeAcc("http://regosp.top:2082/");
+          
+            while(true)
+            {
+                Console.WriteLine("begin y,n?");
+                if (Console.ReadLine().Trim() == "y")
+                {
+                    url = acc.dr.Url;
+                    break;
+                }
+
+            }
+
+            string[] domains = File.ReadAllLines("domain.txt");
+          
+            foreach (string domain in domains)
+            {
+                acc.PutDomain(url,domain);
+               
+            }
+
+
+           
+
+        }
+    }
+}
